09a9aea2ed69b1d8cae5bac1870e1dbf73407f2a
hebruce259 <saundrabruce@hotmail.com>
2016-11-27 19:40:15 -0500

    Adding new web app

---
+++ js/Groceryjs2.js
@@ -0,0 +1,14 @@
+
+function setPrice(){
+
+var pepsiPrice = 1.22
+document.getElementById("Pepsi").value = pepsiPrice;
+
+}
+
+
+//the function body is the same as you have defined sue the textbox object to set the value
+
+//var x = document.getElementById("Pepsi").rows[0].cells.length;
+//alert(document.getElementById("myTable").rows[0].cells.Pepsi(0).innerHTML)
+
